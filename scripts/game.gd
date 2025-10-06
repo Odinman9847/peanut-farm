42a090fb7618b2ace9674b93ba39fa26fca32124
Odinman9847 <100015364+Odinman9847@users.noreply.github.com>
2025-10-05 21:34:42 -0700

    feat: Add peanut that when clicked increments label

---
+++ scripts/game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+@onready var peanut_count: Label = $"Peanut Count"
+var score: int = 0
+
+func _ready():
+	update_label()
+
+func increment_score():
+	score += 1
+	update_label()
+	
+func update_label():
+	peanut_count.text = "Peanuts: " + str(score)
+	
+func _on_peanut_plant_picked() -> void:
+	increment_score()
---
+++ scripts/peanut_plant.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+signal picked
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+			picked.emit()
