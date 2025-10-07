2667235339e6b23a805fc63ecdda738058600e7f
Odinman9847 <100015364+Odinman9847@users.noreply.github.com>
2025-10-07 16:02:57 -0700

    feat: Prices now increase for each peanut plant

--- scripts/game.gd
+++ scripts/game.gd
@@ -13,21 +13,27 @@ var current_peanut_plants: int = 0
 var peanut_count: int = 0
 var peanut_cost: int = 1
 var money: int = 0
-var plant_cost: int = 5
+var plant_costs_array: Array = [5, 10, 20, 60, 120, 256]
+var plant_cost: int = plant_costs_array[0]
 var max_peanut_plants: int
 
 func _ready():
-	update_ui()
 	max_peanut_plants = plant_spawn_points.size()
 	spawn_peanut_plant(plant_spawn_points[0])
+	update_ui()
 	
 func update_ui() -> void:
 	peanut_count_label.text = "Peanuts: " + str(peanut_count)
 	money_label.text = "Money: $" + str(money)
-	buy_plant_label.text = "Peanut Plant: $" + str(plant_cost)
 	
-	buy_plant_button.disabled = (money < plant_cost) or current_peanut_plants >= max_peanut_plants
+	if current_peanut_plants >= max_peanut_plants:
+		buy_plant_label.text = "Out Of Stock!"
+	else:
+		buy_plant_label.text = "Peanut Plant: $" + str(plant_cost)
 
+	buy_plant_button.disabled = (money < plant_cost) or current_peanut_plants >= max_peanut_plants
+	
+	
 func increment_peanut_count():
 	peanut_count += 1
 	update_ui()
@@ -36,7 +42,7 @@ func _on_peanut_plant_picked() -> void:
 	increment_peanut_count()
 
 func sell_peanuts() -> void:
-	money += peanut_count * peanut_cost 
+	money += peanut_count * peanut_cost
 	peanut_count = 0
 	update_ui()
 
@@ -48,8 +54,10 @@ func _on_sell_button_pressed() -> void:
 func _on_buy_plant_button_pressed() -> void:
 	if current_peanut_plants < max_peanut_plants:
 		money -= plant_cost
+		if current_peanut_plants < max_peanut_plants - 1:
+			plant_cost = plant_costs_array[current_peanut_plants]
+			
 		spawn_peanut_plant(plant_spawn_points[current_peanut_plants])
-		print("current_peanut_plants: " + str(current_peanut_plants))
 		update_ui()
 
 func spawn_peanut_plant(marker: Marker2D):
@@ -59,4 +67,3 @@ func spawn_peanut_plant(marker: Marker2D):
 		new_peanut_plant.position = marker.position
 		add_child(new_peanut_plant)
 		current_peanut_plants += 1
-	
