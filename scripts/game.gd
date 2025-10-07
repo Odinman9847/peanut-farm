2b6fbe5a1c910f17444c43b3ebeb23b9289d836e
Odinman9847 <100015364+Odinman9847@users.noreply.github.com>
2025-10-06 18:30:45 -0700

    feat: Add peanut selling

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,17 +1,33 @@
 extends Node2D
-@onready var peanut_count: Label = $"Peanut Count"
+@onready var peanut_count_label: Label = $"Peanut Count"
+@onready var money_label: Label = $Money
 
-var score: int = 0
+var peanut_count: int = 0
+var peanut_cost: int = 1
+var money: int = 0
 
 func _ready():
-	update_label()
+	update_peanut_count_label()
 
-func increment_score():
-	score += 1
-	update_label()
+func increment_peanut_count():
+	peanut_count += 1
+	update_peanut_count_label()
 	
-func update_label():
-	peanut_count.text = "Peanuts: " + str(score)
+func update_peanut_count_label():
+	peanut_count_label.text = "Peanuts: " + str(peanut_count)
+	
+func update_money_label():
+	money_label.text = "Money: $" + str(money)
 	
 func _on_peanut_plant_picked() -> void:
-	increment_score()
+	increment_peanut_count()
+
+func sell_peanuts() -> void:
+	money += peanut_count * peanut_cost 
+	peanut_count = 0
+	update_peanut_count_label()
+	update_money_label()
+
+
+func _on_sell_button_pressed() -> void:
+	sell_peanuts()
