81a51554fc60b669accb84193882e9380f2b57ce
Odinman9847 <100015364+Odinman9847@users.noreply.github.com>
2025-10-06 19:01:34 -0700

    feat: Add store to buy plant, only subtracts money now

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,23 +1,26 @@
 extends Node2D
-@onready var peanut_count_label: Label = $"Peanut Count"
-@onready var money_label: Label = $Money
+@onready var peanut_count_label: Label = $"CanvasLayer/Top Bar/Peanut Count"
+@onready var money_label: Label = $"CanvasLayer/Top Bar/Money"
+@onready var buy_plant_button: Button = $"CanvasLayer/Right Bar/Buy Plant/Buy Plant Button"
+@onready var buy_plant_label: Label = $"CanvasLayer/Right Bar/Buy Plant/Buy Plant Label"
+
 
 var peanut_count: int = 0
 var peanut_cost: int = 1
 var money: int = 0
+var plant_cost: int = 5
 
 func _ready():
-	update_peanut_count_label()
+	update_ui()
+	
+func update_ui() -> void:
+	peanut_count_label.text = "Peanuts: " + str(peanut_count)
+	money_label.text = "Money: $" + str(money)
+	buy_plant_label.text = "Peanut Plant: $" + str(plant_cost)
 
 func increment_peanut_count():
 	peanut_count += 1
-	update_peanut_count_label()
-	
-func update_peanut_count_label():
-	peanut_count_label.text = "Peanuts: " + str(peanut_count)
-	
-func update_money_label():
-	money_label.text = "Money: $" + str(money)
+	update_ui()
 	
 func _on_peanut_plant_picked() -> void:
 	increment_peanut_count()
@@ -25,9 +28,14 @@ func _on_peanut_plant_picked() -> void:
 func sell_peanuts() -> void:
 	money += peanut_count * peanut_cost 
 	peanut_count = 0
-	update_peanut_count_label()
-	update_money_label()
+	update_ui()
 
 
 func _on_sell_button_pressed() -> void:
 	sell_peanuts()
+
+
+func _on_buy_plant_button_pressed() -> void:
+	if money >= plant_cost:
+		money -= plant_cost
+		update_ui()
